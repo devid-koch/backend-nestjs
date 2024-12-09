202c847f229e7fdf2261bd876008ca4e2ba8f0b8
deviid123 <devidkoch321@gmail.com>
2024-12-09 20:30:08 +0530

    Configure database connection with TypeORM, Add JWT,Google OAuth, LinkedIn OAuth configuration files

---
+++ src/config/linkedin.config.ts
@@ -0,0 +1,5 @@
+export const linkedinConfig = {
+  clientID: process.env.LINKEDIN_CLIENT_ID,
+  clientSecret: process.env.LINKEDIN_CLIENT_SECRET,
+  callbackURL: process.env.LINKEDIN_CALLBACK_URL,
+};
